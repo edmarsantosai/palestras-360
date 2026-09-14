4f8b6be321e38a36179590369cd9c1d2a9963433
Edmar Santos <edmarsantos.unasp@gmail.com>
2026-09-14 01:10:13 -0300

    fix(logos): transparentes _raw, sem figcaption, tamanho 96-100px, 4-5 colunas

--- assets/js/render-lp.js
+++ assets/js/render-lp.js
@@ -744,7 +744,7 @@ function buildProvaSocialStrip() {
   const logos = primeiros12.map((l, i) => {
     const loading = i < 4 ? 'eager' : 'lazy';
     return `          <li class="prova-strip__logo">
-            <img src="/assets/img/prova-social/${esc(l.arquivo_png)}"
+            <img src="/assets/img/prova-social/_raw/${esc(l.arquivo_png)}"
                  alt="${esc(l.alt_seo)}"
                  title="${esc(l.title_seo)}"
                  width="240" height="80"
@@ -781,12 +781,11 @@ function buildProvaSocial() {
     const loading = i < 8 ? 'eager' : 'lazy';
     return `        <li class="prova-grid__item">
           <figure>
-            <img src="/assets/img/prova-social/${esc(l.arquivo_png)}"
+            <img src="/assets/img/prova-social/_raw/${esc(l.arquivo_png)}"
                  alt="${esc(l.alt_seo)}"
-                 title="${esc(l.title_seo)}"
+                 title="${esc(l.marca)}"
                  width="240" height="80"
                  loading="${loading}" decoding="async">
-            <figcaption>${esc(l.figcaption)}</figcaption>
           </figure>
         </li>`;
   }).join('\n');
